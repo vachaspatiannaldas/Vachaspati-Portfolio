56b29221b11e91b6808a5c0d38b44ca9e08a31ed
unknown <annaldasvachaspati@gmail.com>
2024-04-30 13:30:21 +0530

    portfolio

---
+++ src/Details.js
@@ -0,0 +1,180 @@
+// Logo images
+
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+import nodejs from "./assets/techstack/nodejs.png";
+import mongodb from "./assets/techstack/mongodb.png";
+import mysql from "./assets/techstack/mysql.png";
+import nextjs from "./assets/techstack/nextjs.png";
+import laravel from "./assets/techstack/laravel.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.png";
+import projectImage4 from "./assets/projects/project4.png";
+import projectImage5 from "./assets/projects/project5.png";
+import projectImage6 from "./assets/projects/project6.png";
+import projectImage7 from "./assets/projects/project7.png";
+
+
+//Resume
+import resume from "./assets/Vachaspati_Annaldas_Resume.pdf";
+
+
+
+export const personalDetails = {
+  name: "Vachaspati Annaldas",
+  tagline: "Junior Full Stack Developer",
+  img: profile,
+  about: `As a Junior Full Stack Web Developer, I have a strong foundation in a range of technologies. My expertise spans from front-end development to back-end programming. One of my favourite technologies to work with is React.js, particularly within the MERN stack.
+  Eager to collaborate and explore exciting opportunities in the world of web development. #FullStack #WebDevelopment #ReactJS #NodeJS #ExpressJS #MongoDB`,
+};
+
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/vachaspati-annaldas/",
+  github: "https://github.com/vachaspatiannaldas/",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+export const workDetails = [
+  {
+    Position: "Junior Fullstack Developer",
+    Company: `Vertex Technosys`,
+    Location: "Solapur",
+    Type: "Full Time",
+    Duration: "Jul 2023 - Present",
+  },
+  {
+    Position: "Frontend Development Intern",
+    Company: `Vertex Technosys`,
+    Location: "Solapur",
+    Type: "Internship",
+    Duration: "Jan 2023 - Jun 2023",
+  },
+];
+
+export const eduDetails = [
+  {
+    Position: "BTech in Computer Science Engineering ",
+    Company: "Bharat Ratna Indira Gandhi College of Engineering",
+    Location: "Solapur",
+    Type: "Full Time",
+    Duration: "Jan 2020 - Jul 2023",
+  },
+  {
+    Position: "Diploma in Computer Technology ",
+    Company: `Solapur Education Society Polytechnic`,
+    Location: "Solapur",
+    Type: "Full Time",
+    Duration: "2016 - 2019",
+  },
+];
+
+export const resumeDetails = {
+  resume:resume,
+}
+
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+  nodejs:nodejs,
+  mongodb:mongodb,
+  mysql:mysql,
+  nextjs:nextjs,
+  laravel:laravel,
+};
+
+export const projectDetails = [
+  {
+    title: "TrendSays",
+    image: projectImage7,
+    description: `TrendSays, a dynamic blog website showcasing trending topics in technology, lifestyle, science, entertainment,
+    space, and education.`,
+    techstack: "React.js, Node.js, Express.js, MongoDB (MERN)",
+    previewLink: "https://trendsay.vercel.app/",
+    githubLink: "https://github.com/vachaspatiannaldas/trendsays.",
+  },
+  {
+    title: "Cilory",
+    image: projectImage1,
+    description: `Cilory clone website offering online clothes shopping with categories for men and women, featuring t-shirts, shirts, and jeans.`,
+    techstack: "React.js, Node.js, Express.js, MongoDB (MERN)",
+    previewLink: "https://deployments-eta.vercel.app/",
+    githubLink: "https://github.com/vachaspatiannaldas/Cilory-clone",
+  },
+  {
+    title: "MyFunctionHall",
+    image: projectImage6,
+    description: `Book function halls for weddings, birthdays, engagements, and special events conveniently online. Simplifying event planning with ease.`,
+    techstack: "React.js, Laravel, MySQL, Bootstrap, CSS3",
+    previewLink: "https://bookmyfunctionhall.com/",
+    githubLink: "https://github.com/vachaspatiannaldas/MyFunctionHall",
+  },
+  {
+    title: "OGMart",
+    image: projectImage2,
+    description: `Your go-to online store for electronics including smartphones, smartwatches, headphones, and trendy clothing for both men and women.`,
+    techstack: "React.js, Laravel, MySQL, Bootstrap, CSS3",
+    previewLink: "https://github.com/vachaspatiannaldas/OGMart-Project",
+    githubLink: "https://github.com/vachaspatiannaldas/OGMart-Project",
+  },
+  {
+    title: "BMH (Book My Hotel)",
+    image: projectImage3,
+    description: `Effortlessly reserve rooms in 3 to 7-star hotels across India. Streamlining your hotel booking experience online.`,
+    techstack: "Next.js, Laravel, MySQL, Bootstrap, CSS3",
+    previewLink: "https://github.com/vachaspatiannaldas/BMH",
+    githubLink: "https://github.com/vachaspatiannaldas/BMH",
+  },
+  {
+    title: "BMH Admin Panel",
+    image: projectImage4,
+    description: `Admin panel enables easy management of rooms, available dates, sectors, and types for efficient hotel room booking services.`,
+    techstack: "Next.js, Laravel, MySQL, Bootstrap, CSS3",
+    previewLink: "https://github.com/vachaspatiannaldas/BMH/tree/master/BMH-Adminpanel",
+    githubLink: "https://github.com/vachaspatiannaldas/BMH/tree/master/BMH-Adminpanel",
+  },
+  {
+    title: "OGMart Admin Panel",
+    image: projectImage5,
+    description: `Empowering administrators with seamless control to add, manage, and display products, categories, and subcategories effortlessly.`,
+    techstack: "React.js, Laravel, MySQL, Bootstrap, CSS3",
+    previewLink: "https://github.com/vachaspatiannaldas/OGMart-Project/tree/master/OGMart_admin",
+    githubLink: "https://github.com/vachaspatiannaldas/OGMart-Project/tree/master/OGMart_admin",
+  },
+  
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "annaldasvachaspati@gmail.com",
+  phone: "+91 70202 70105",
+};
